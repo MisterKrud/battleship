6e5b537bdfa35af53b66e460ed922c43c94bb9a9
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-07 16:53:53 +1100

    wip: Add mostly empty gameboard module

---
+++ gameboard.js
@@ -0,0 +1,13 @@
+const gameBoard = (rows, cols)=>{
+    
+    const board = (rows = 10, cols = 10) => {
+        const boardArray = []
+        for(let i = 0; i<rows; i++){
+            for(let j=0; j< cols; j++){
+                boardArray.push[i][j]
+            }
+            return boardArray
+
+        }
+    }
+}
