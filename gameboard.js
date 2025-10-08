6dc28638cc50abb024b5a4eb3609104a644380ca
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-08 13:39:41 +1100

    wip: Add placeShip function to gameboard
    This function places the ship at a set of coordinates.
    It doesn't yet:
    A - Check if the ship will place outside the board edges
    B - Allow for vertical placement (currently only rows)

--- gameboard.js
+++ gameboard.js
@@ -1,13 +1,52 @@
-const gameBoard = (rows, cols)=>{
+const Ship = require("./battleship")
+
+const gameboard = (rows = 10, cols = 10)=>{
     
-    const board = (rows = 10, cols = 10) => {
+    const getBoard = () => {
         const boardArray = []
         for(let i = 0; i<rows; i++){
+            boardArray[i]=[]
             for(let j=0; j< cols; j++){
-                boardArray.push[i][j]
+                boardArray[i].push([`${i},${j}`])
             }
-            return boardArray
+        }
+        return boardArray
+    }
+
+    const board = getBoard(rows, cols)
+
+    
+        const carrier = new Ship(5, "Carrier")
+        const battleship = new Ship(4, "Battleship")
+        const cruiser = new Ship(3, "Cruiser")
+        const submarine = new Ship(3, "Submarine")
+        const destroyer = new Ship(2, "Destroyer")
+
+    const placeShip = (ship, row, col) => {
+        let arr = [];
+        const shipName = (`${ship.shipName}`).split('');
+        const shipAbbrev = `${shipName[0]}${shipName[1]}${shipName[2]}`
+        for(l=0; l<ship.shipLength; l++){
+            arr.push(shipAbbrev)
+            board[row][col+l] = [shipAbbrev]
 
         }
+        return shipAbbrev
+    
     }
+
+        
+
+
+
+
+
+
+    return {board, carrier, battleship, cruiser, submarine, destroyer, placeShip}
 }
+
+
+
+
+
+module.exports = gameboard
