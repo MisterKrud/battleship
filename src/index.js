8aef6254a03acdbde9720aab92f872c506e2e329
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-09 12:42:14 +1100

    wip: Add webpack and create basic css for board

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css"
+
 const Ship = require("./battleship")
 const Gameboard = require("./gameboard");
 const Player = require("./player");
@@ -7,17 +9,19 @@ const player1Board = player.board;
 const Player2Board = player.board;
 const body = document.querySelector("body")
 const playerOneBoard = document.getElementById("player-one-board");
-playerOneBoard.textContent = 'text'
+
 const renderBoard = () => {
     for(let i=0; i< 10; i++){
     const row = document.createElement("div");
     
      playerOneBoard.appendChild(row);
-     row.textContent = "row"
+     row.classList.add("row")
+    
      for(let j=0; j< 10; j++){
         const col = document.createElement("div");
+        col.classList.add("cell")
         row.appendChild(col)
-        col.textContent ='test'//`${i},${j}`
+        col.textContent =`${i}, ${j}`
      }
     }
 }
