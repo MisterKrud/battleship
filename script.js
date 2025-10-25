9c238a0a1767136673c9535c06fa05a0135727c0
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-25 18:11:34 +1100

    Updates

--- script.js
+++
@@ -1,3 +0,0 @@
-const Ship = require("./battleship")
-const Gameboard = require("./gameboard");
-const Player = require("./player");
--- tests/player.test.js
+++
@@ -1,7 +0,0 @@
-const Player = require("../src/player");
-const player = require("../src/player");
-const player1 = Player()
-const player1Board = player1.board
-test(`Check player board is correctly returning from factory`,()=>{
-    expect(player1.board.length).toBe(10)
-})
